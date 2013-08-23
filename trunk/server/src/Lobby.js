ea96ad27d764729499c44cbf2d19041634a0c8b0
brakok@gmail.com <brakok@gmail.com@bb0c4cdd-5914-11bf-209a-e84db34162ea>
2013-08-23 15:56:59 +0000

    New lobby features.

--- trunk/server/src/Lobby.js
+++ trunk/server/src/Lobby.js
@@ -3,15 +3,46 @@ var Lobby = function(id, username){
 
 	this.id = id;
 	this.connectedPlayers = 1;
-	this.host = username;
+	
+	this.players = [];
+	this.addPlayer(username, Enum.Slot.Color.UNASSIGNED);
 	
 	this.settings = new GameSettings(id, 1200, 800, 2);
 };
 
+Lobby.prototype.addPlayer = function(username, color){
+	this.players.push({
+		username: username,
+		color: color,
+		ready: false
+	});
+};
+
+Lobby.prototype.removePlayer = function(username){
+	
+	var index = null;
+	for(var i in this.players)	
+		if(this.players[i].username == username)
+		{
+			index = i;
+			break;
+		}
+	
+	if(index != null)
+		this.players.splice(index, 1);
+};
+
 Lobby.prototype.toClient = function(){
+
+	var players = [];
+	
+	for(var i in this.players)
+		if(this.players[i] != null)
+			players.push(this.players[i]);
 
 	return {
 		id: this.id,
+		players: players,
 		settings: this.settings,
 		connectedPlayers: this.connectedPlayers
 	};
