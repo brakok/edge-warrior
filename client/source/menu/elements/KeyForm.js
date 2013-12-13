bfbb6f4a6bd284cbdbb9f822872e3cccb0fbc99d
brakok@gmail.com <brakok@gmail.com@bb0c4cdd-5914-11bf-209a-e84db34162ea>
2013-12-13 18:03:59 +0000

    Added new keys to buy skills.

--- client/source/menu/elements/KeyForm.js
+++ client/source/menu/elements/KeyForm.js
@@ -14,6 +14,13 @@ var KeyForm = function(){
 	var txtKeyOpt2 = new KeyInput('keyOpt2', this.keys.OPT2, this);
 	var txtKeyPause = new KeyInput('keyPause', this.keys.PAUSE, this);
 	
+	var txtKeySkill1 = new KeyInput('keySkill1', this.keys.SKILL1, this);
+	var txtKeySkill2 = new KeyInput('keySkill2', this.keys.SKILL2, this);
+	var txtKeySkill3 = new KeyInput('keySkill3', this.keys.SKILL3, this);
+	var txtKeySkill4 = new KeyInput('keySkill4', this.keys.SKILL4, this);
+	
+	var txtKeyBuyMode = new KeyInput('keyBuyMode', this.keys.TOGGLE_BUY_MODE, this);
+	
 	//Update keys for new ones.
 	this.updateKeys = function(){
 		this.keys.RIGHT = txtKeyRight.value;
@@ -23,6 +30,13 @@ var KeyForm = function(){
 		this.keys.OPT1 = txtKeyOpt1.value;
 		this.keys.OPT2 = txtKeyOpt2.value;
 		this.keys.PAUSE = txtKeyPause.value;
+		
+		this.keys.SKILL1 = txtKeySkill1.value;
+		this.keys.SKILL2 = txtKeySkill2.value;
+		this.keys.SKILL3 = txtKeySkill3.value;
+		this.keys.SKILL4 = txtKeySkill4.value;
+		
+		this.keys.TOGGLE_BUY_MODE = txtKeyBuyMode.value;
 	};
 	
 	//Save keys.
@@ -48,7 +62,12 @@ var KeyForm = function(){
 		   || this.keys.KILL == code
 		   || this.keys.OPT1 == code
 		   || this.keys.OPT2 == code
-		   || this.keys.PAUSE == code)
+		   || this.keys.PAUSE == code
+		   || this.keys.SKILL1 == code
+		   || this.keys.SKILL2 == code
+		   || this.keys.SKILL3 == code
+		   || this.keys.SKILL4 == code
+		   || this.keys.TOGGLE_BUY_MODE == code)
 		 {
 			return false;
 		 }
@@ -58,6 +77,7 @@ var KeyForm = function(){
 		
 	//Set values in inputs.
 	this.load = function(){
+	
 		txtKeyRight.setValue(this.keys.RIGHT, false);
 		txtKeyLeft.setValue(this.keys.LEFT, false);
 		txtKeyJump.setValue(this.keys.JUMP, false);
@@ -65,6 +85,13 @@ var KeyForm = function(){
 		txtKeyOpt1.setValue(this.keys.OPT1, false);
 		txtKeyOpt2.setValue(this.keys.OPT2, false);
 		txtKeyPause.setValue(this.keys.PAUSE, false);
+		
+		txtKeySkill1.setValue(this.keys.SKILL1, false);
+		txtKeySkill2.setValue(this.keys.SKILL2, false);
+		txtKeySkill3.setValue(this.keys.SKILL3, false);
+		txtKeySkill4.setValue(this.keys.SKILL4, false);
+		
+		txtKeyBuyMode.setValue(this.keys.TOGGLE_BUY_MODE, false);
 	};
 	
 	this.init = function(){
