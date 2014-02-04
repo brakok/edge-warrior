fe1597e65416c5f8771d3c925d53a3a1ca3e2fdf
brakok@gmail.com <brakok@gmail.com@bb0c4cdd-5914-11bf-209a-e84db34162ea>
2014-02-04 00:19:27 +0000

    Start: drag-n-drop with skills.

--- trunk/client/source/playground/skills/SkillDescription.js
+++ trunk/client/source/playground/skills/SkillDescription.js
@@ -19,11 +19,9 @@ var SkillDescription = function(type){
 	this.selected = false;
 	
 	this.layer = null;
-	this.callback = null;
 	
 	this.description = null;
 	this.sprite = null;
-	this.menu = null;
 	
 	this.cost = null;
 	this.percent = {
@@ -56,7 +54,7 @@ SkillDescription.prototype.init = function(){
 	}
 };
 
-SkillDescription.prototype.load = function(x, y, scaleX, scaleY, layer, callback){
+SkillDescription.prototype.load = function(x, y, scaleX, scaleY, layer){
 
 	this.x = x;
 	this.y = y;
@@ -67,42 +65,18 @@ SkillDescription.prototype.load = function(x, y, scaleX, scaleY, layer, callback
 	};
 	
 	this.layer = layer;
-	this.callback = callback;
 	
-	if(this.callback == null)
-	{
-		//Create right image.
-		switch(this.type){
-			case Enum.Block.Skill.FIRE_PULSE:
-				this.sprite = cc.Sprite.create(assetsSkillDir + 'firePulse.png');				
-				break;
-		}
-	}
-	else
-	{
-		//Create right image.
-		switch(this.type){
-			case Enum.Block.Skill.FIRE_PULSE:
-				this.sprite = new cc.MenuItemImage.create(assetsSkillDir + 'firePulse.png',
-														  assetsSkillDir + 'firePulse.png',
-														  this.callback,
-														  this)				
-				break;
-		}
+	//Create right image.
+	switch(this.type){
+		case Enum.Block.Skill.FIRE_PULSE:
+			this.sprite = cc.Sprite.create(assetsSkillDir + 'firePulse.png');				
+			break;
 	}
 	
 	this.sprite.setPosition(new cc.Point(this.x, this.y));
 	this.sprite.setScale(this.scale.x, this.scale.y);
 	
-	if(this.callback == null)
-		this.layer.addChild(this.sprite);
-	else
-	{
-		this.menu = new cc.Menu.create(this.sprite);
-		this.menu.setPosition(new cc.Point(0,0));
-		
-		this.layer.addChild(this.menu);
-	}
+	this.layer.addChild(this.sprite);
 };
 
 SkillDescription.prototype.unselect = function(){
@@ -119,18 +93,12 @@ SkillDescription.prototype.select = function(){
 	this.remove();
 	this.layer.addChild(this.background);
 	
-	if(this.callback == null)
-		this.layer.addChild(this.sprite);
-	else
-		this.layer.addChild(this.menu);
+	this.layer.addChild(this.sprite);
 };
 
 SkillDescription.prototype.remove = function(){
 
-	if(this.callback == null)
-		this.layer.removeChild(this.sprite);
-	else
-		this.layer.removeChild(this.menu);
+	this.layer.removeChild(this.sprite);
 };
 
 //Return list of all skills.
