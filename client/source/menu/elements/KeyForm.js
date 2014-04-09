73c5a3c127deb000051e96fd7b825bf0f9fb9f3f
brakok@gmail.com <brakok@gmail.com@bb0c4cdd-5914-11bf-209a-e84db34162ea>
2014-04-09 01:24:05 +0000

    Comment kill command.

--- client/source/menu/elements/KeyForm.js
+++ client/source/menu/elements/KeyForm.js
@@ -8,7 +8,7 @@ var KeyForm = function(){
 	var txtKeyLeft = new KeyInput('keyLeft', this.keys.LEFT, this);
 	
 	var txtKeyJump = new KeyInput('keyJump', this.keys.JUMP, this);
-	var txtKeyKC = new KeyInput('keyKC', this.keys.KILL, this);
+	//var txtKeyKC = new KeyInput('keyKC', this.keys.KILL, this);
 	
 	var txtKeyOpt1 = new KeyInput('keyOpt1', this.keys.OPT1, this);
 	var txtKeyOpt2 = new KeyInput('keyOpt2', this.keys.OPT2, this);
@@ -29,7 +29,7 @@ var KeyForm = function(){
 		this.keys.RIGHT = txtKeyRight.value;
 		this.keys.LEFT = txtKeyLeft.value;
 		this.keys.JUMP = txtKeyJump.value;
-		this.keys.KILL = txtKeyKC.value;
+		//this.keys.KILL = txtKeyKC.value;
 		this.keys.OPT1 = txtKeyOpt1.value;
 		this.keys.OPT2 = txtKeyOpt2.value;
 		this.keys.PAUSE = txtKeyPause.value;
@@ -65,7 +65,7 @@ var KeyForm = function(){
 		if(this.keys.RIGHT == code
 		   || this.keys.LEFT == code
 		   || this.keys.JUMP == code
-		   || this.keys.KILL == code
+		   //|| this.keys.KILL == code
 		   || this.keys.OPT1 == code
 		   || this.keys.OPT2 == code
 		   || this.keys.PAUSE == code
@@ -90,7 +90,7 @@ var KeyForm = function(){
 		txtKeyRight.setValue(this.keys.RIGHT, false);
 		txtKeyLeft.setValue(this.keys.LEFT, false);
 		txtKeyJump.setValue(this.keys.JUMP, false);
-		txtKeyKC.setValue(this.keys.KILL, false);
+		//txtKeyKC.setValue(this.keys.KILL, false);
 		txtKeyOpt1.setValue(this.keys.OPT1, false);
 		txtKeyOpt2.setValue(this.keys.OPT2, false);
 		txtKeyPause.setValue(this.keys.PAUSE, false);
