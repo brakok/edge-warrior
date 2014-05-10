87250593ff2343cdb7ec6fc30971534ecd72e2a1
loredana.tepelus@hotmail.com <loredana.tepelus@hotmail.com@bb0c4cdd-5914-11bf-209a-e84db34162ea>
2014-05-10 07:52:35 +0000

    Basic integration done.

--- website/Script/slideshow.js
+++ website/Script/slideshow.js
@@ -4,3 +4,18 @@ var mySwiper = new Swiper('.swiper-container',{
     slidesPerView: 3,
     loop: true
   })
+
+  var mySwiper = $('.swiper-container').swiper({
+  slidesPerView:3
+})
+$(window).resize(function(){
+  var ww = $(window).width()
+  if (ww>1600) mySwiper.params.slidesPerView = 3;
+  if (ww<=1100) mySwiper.params.slidesPerView = 2;
+  if (ww<=800) mySwiper.params.slidesPerView = 1;
+  mySwiper.reInit()
+})
+$(window).trigger('resize');
+  
+  
+  
